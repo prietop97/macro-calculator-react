a7812d4d53aa5e48c90bb0ac6f6d04d1c1a147dd
Pedro Prieto <pedro.prieto003@gmail.com>
2020-02-02 07:54:38 -0500

    Fixed calculations bug

--- src/Redux/UserStats/userStatsActions.js
+++ src/Redux/UserStats/userStatsActions.js
@@ -1,113 +1,131 @@
-import axiosWithAuth from "../../Utils/axiosWithAuth"
+import axiosWithAuth from '../../Utils/axiosWithAuth';
 
-export const FETCH_STATS_START = "FETCH_STATS_START"
-export const FETCH_STATS_SUCCESS = "FETCH_STATS_SUCCESS"
-export const FETCH_STATS_FAILED = "FETCH_STATS_FAILED"
+export const FETCH_STATS_START = 'FETCH_STATS_START';
+export const FETCH_STATS_SUCCESS = 'FETCH_STATS_SUCCESS';
+export const FETCH_STATS_FAILED = 'FETCH_STATS_FAILED';
 
-export const SEND_STATS_START = "SEND_STATS_START"
-export const SEND_STATS_SUCCESS = "SEND_STATS_SUCCESS"
-export const SEND_STATS_FAILED = "SEND_STATS_FAILED"
+export const SEND_STATS_START = 'SEND_STATS_START';
+export const SEND_STATS_SUCCESS = 'SEND_STATS_SUCCESS';
+export const SEND_STATS_FAILED = 'SEND_STATS_FAILED';
 
-export const EDIT_STATS_START = "EDIT_STATS_START"
-export const EDIT_STATS_SUCCESS = "EDIT_STATS_SUCCESS"
-export const EDIT_STATS_FAILED = "EDIT_STATS_FAILED"
+export const EDIT_STATS_START = 'EDIT_STATS_START';
+export const EDIT_STATS_SUCCESS = 'EDIT_STATS_SUCCESS';
+export const EDIT_STATS_FAILED = 'EDIT_STATS_FAILED';
 
-export const sendStats = (info,history) => dispatch => {
-    dispatch({type:SEND_STATS_START})
-    axiosWithAuth()
-        .post("/info",info)
-        .then(res=>{
-            dispatch({type: SEND_STATS_SUCCESS , payload: res.data})
-            history.push("/Calculatedpage")
-            })
-        .catch(err=>console.log(err))
-        
-}
+export const sendStats = (info, history) => dispatch => {
+  dispatch({ type: SEND_STATS_START });
+  axiosWithAuth()
+    .post('/info', info)
+    .then(res => {
+        const cal = calculateStats(res.data);
+      const protein = Math.floor(cal * 0.075);
+      const carbs = Math.floor(cal * 0.1);
+      const fats = Math.floor(cal * 0.033);
+      const macro = mealPlan(protein, carbs, fats, res.data);
+      dispatch({
+        type: SEND_STATS_SUCCESS,
+        payload: {
+          data: res.data,
+          calculations: { cal, protein, carbs, fats, macro }
+        }
+      });
+      history.push('/Calculatedpage');
+    })
+    .catch(err => console.log(err));
+};
 
-export const fetchStats = (id) => dispatch => {
-    dispatch({type:FETCH_STATS_START})
-    axiosWithAuth()
-        .get(`/info`)
-        .then(res=>dispatch({type: FETCH_STATS_SUCCESS , payload: res.data}))
-        .catch(err=>dispatch({type: FETCH_STATS_FAILED}))
-}
+export const fetchStats = id => dispatch => {
+  dispatch({ type: FETCH_STATS_START });
+  axiosWithAuth()
+    .get(`/info`)
+    .then(res => {
+      const cal = calculateStats(res.data);
+      const protein = Math.floor(cal * 0.075);
+      const carbs = Math.floor(cal * 0.1);
+      const fats = Math.floor(cal * 0.033);
+      const macro = mealPlan(protein, carbs, fats, res.data);
+      dispatch({
+        type: FETCH_STATS_SUCCESS,
+        payload: {
+          data: res.data,
+          calculations: { cal, protein, carbs, fats, macro }
+        }
+      });
+    })
+    .catch(err => dispatch({ type: FETCH_STATS_FAILED }));
+};
 
-export const editData = (id,formValues,history) => dispatch => {
-    axiosWithAuth()
-        .put(`/info/${id}`,formValues)
-        .then(()=>{
-            history.push("/Dashboard")
-        })
-        .catch(err=>console.log(err))
-    
-}
+export const editData = (id, formValues, history) => dispatch => {
+  axiosWithAuth()
+    .put(`/info/${id}`, formValues)
+    .then(() => {
+      history.push('/Dashboard');
+    })
+    .catch(err => console.log(err));
+};
 
 ///// NOT REASIB TO USE THIS !! ONLY FOR REACT 2 MVP ---
-export const deleteStats = (id,history) => dispatch => {
-    axiosWithAuth()
-        .delete(`/info/${id}`)
-        .then(()=>history.push("/Biometric"))
-        .catch(err=>console.log(err))
-}
+export const deleteStats = (id, history) => dispatch => {
+  axiosWithAuth()
+    .delete(`/info/${id}`)
+    .then(() => history.push('/Biometric'))
+    .catch(err => console.log(err));
+};
 
-export const calculateStats = (user) => {
-    let day = ""
-    let month = ""
-    if(user.birthdate_day <= 9){
-        day = `0${user.birthdate_day}`
-    }else{
-        day = `${user.birthdate_day}`
-    }
+export const calculateStats = user => {
+  let day = '';
+  let month = '';
+  if (user.birthdate_day <= 9) {
+    day = `0${user.birthdate_day}`;
+  } else {
+    day = `${user.birthdate_day}`;
+  }
 
-    if(user.birthdate_month <= 9){
-        month = `0${user.birthdate_month}`
-    }else{
-        month = `${user.birthdate_month}`
-    }
+  if (user.birthdate_month <= 9) {
+    month = `0${user.birthdate_month}`;
+  } else {
+    month = `${user.birthdate_month}`;
+  }
 
-    let currentDate = new Date()
-    let birthDate = new Date(`${month}/${day}/${user.birthdate_year}`)
-    let oneYear = 1000 * 60 * 60 * 24 * 365
-    let difference = Math.floor((currentDate - birthDate) / oneYear)
-    let BMR = 0
-    if(user.gender === "male"){
-        BMR = 66 + (6.23 * user.weight) + (12.7 * user.height) - (6.8 * difference)
-    }else{
-        BMR = 655 + (4.35 * user.weight) + (4.7 * user.height) - (4.5 * difference)
-    }
-    return Math.floor(BMR * user.activity_factor * user.goal_multiplier)
-}
+  let currentDate = new Date();
+  let birthDate = new Date(`${month}/${day}/${user.birthdate_year}`);
+  let oneYear = 1000 * 60 * 60 * 24 * 365;
+  let difference = Math.floor((currentDate - birthDate) / oneYear);
+  let BMR = 0;
+  if (user.gender === 'male') {
+    BMR = 66 + 6.23 * user.weight + 12.7 * user.height - 6.8 * difference;
+  } else {
+    BMR = 655 + 4.35 * user.weight + 4.7 * user.height - 4.5 * difference;
+  }
+  return Math.floor(BMR * user.activity_factor * user.goal_multiplier);
+};
 
-export const mealPlan = (proteinGrams,carbsGrams,fatsGrams,userInfo) => {
-    let mealProtein = proteinGrams
-    let mealCarbs = carbsGrams
-    let mealFats = fatsGrams
-    let snackProtein = proteinGrams
-    let snackCarbs = carbsGrams
-    let snackFats = fatsGrams
+export const mealPlan = (proteinGrams, carbsGrams, fatsGrams, userInfo) => {
+  let mealProtein = proteinGrams;
+  let mealCarbs = carbsGrams;
+  let mealFats = fatsGrams;
+  let snackProtein = proteinGrams;
+  let snackCarbs = carbsGrams;
+  let snackFats = fatsGrams;
 
-    if(userInfo.snacks_per_day === 0) {
-        mealProtein = Math.floor(mealProtein / userInfo.meals_per_day)
-        mealCarbs = Math.floor(mealCarbs / userInfo.meals_per_day)
-        mealFats = Math.floor(mealFats / userInfo.meals_per_day)
-    }else{
-        snackProtein = Math.floor(mealProtein / 8)
-        mealProtein = Math.floor(snackProtein * 2) 
-        snackCarbs = Math.floor(mealCarbs / 8)
-        mealCarbs = Math.floor(snackCarbs * 2)
-        snackFats = Math.floor(mealFats / 8)
-        mealFats = Math.floor(snackFats * 2)
-
-    }   
-    return {
-          snackProtein,
-          snackFats,
-          snackCarbs,
-          mealProtein,
-          mealCarbs,
-          mealFats
-    }
-
-}
-
-
+  if (userInfo.snacks_per_day === 0) {
+    mealProtein = Math.floor(mealProtein / userInfo.meals_per_day);
+    mealCarbs = Math.floor(mealCarbs / userInfo.meals_per_day);
+    mealFats = Math.floor(mealFats / userInfo.meals_per_day);
+  } else {
+    snackProtein = Math.floor(mealProtein / 8);
+    mealProtein = Math.floor(snackProtein * 2);
+    snackCarbs = Math.floor(mealCarbs / 8);
+    mealCarbs = Math.floor(snackCarbs * 2);
+    snackFats = Math.floor(mealFats / 8);
+    mealFats = Math.floor(snackFats * 2);
+  }
+  return {
+    snackProtein,
+    snackFats,
+    snackCarbs,
+    mealProtein,
+    mealCarbs,
+    mealFats
+  };
+};
--- src/Redux/UserStats/userStatsReducer.js
+++ src/Redux/UserStats/userStatsReducer.js
@@ -1,84 +1,79 @@
 import {
-    FETCH_STATS_FAILED,
-    FETCH_STATS_SUCCESS,
-    FETCH_STATS_START,
-    SEND_STATS_START,
-    SEND_STATS_SUCCESS,
-    SEND_STATS_FAILED
-} from "./userStatsActions"
-import { calculateStats } from "./userStatsActions"
-import { mealPlan } from "./userStatsActions"
+  FETCH_STATS_FAILED,
+  FETCH_STATS_SUCCESS,
+  FETCH_STATS_START,
+  SEND_STATS_START,
+  SEND_STATS_SUCCESS,
+  SEND_STATS_FAILED
+} from './userStatsActions';
+import { calculateStats } from './userStatsActions';
+import { mealPlan } from './userStatsActions';
 
 const initialState = {
-    userStatsObj: {},
-    isFetching: false,
-    error: null,
-    totalCalories: "",
-    proteinGrams : "",
-    carbsGrams : "",
-    fatsGrams : "",
-    Macros: {}
-}
+  userStatsObj: {},
+  isFetching: false,
+  error: null,
+  totalCalories: '',
+  proteinGrams: '',
+  carbsGrams: '',
+  fatsGrams: '',
+  Macros: {}
+};
 
-
-export const userStatsReducer = (state = initialState , {type , payload}) => {
-    switch(type){
-        case FETCH_STATS_START:
-            return {
-                ...state,
-                isFetching: true,
-                error: null,
-                userStatsObj: {}
-            }
-            case FETCH_STATS_SUCCESS:
-                const cals = calculateStats(payload)
-                const Macro = mealPlan(state.proteinGrams,state.carbsGrams,state.fatsGrams,payload)
-                return {
-                ...state,
-                isFetching: false,
-                userStatsObj: payload,
-                totalCalories: cals,
-                proteinGrams: Math.floor(cals * 0.075),
-                carbsGrams: Math.floor(cals * 0.1),
-                fatsGrams: Math.floor(cals * 0.033),
-                Macros: Macro
-            }
-        case FETCH_STATS_FAILED:
-            return {
-                ...state,
-                isFetching: false,
-                error: payload,
-                userStatsObj: {}
-            }
-        case SEND_STATS_START:
-            return{
-                ...state,
-                isFetching: true,
-                error: null,
-                userStatsObj: {}
-            }
-        case SEND_STATS_SUCCESS:
-                const cal = calculateStats(payload)
-                const Macros = mealPlan(state.proteinGrams,state.carbsGrams,state.fatsGrams,payload)
-            return{
-                ...state,
-                isFetching: false,
-                error: null,
-                userStatsObj: payload,
-                totalCalories: cal,
-                proteinGrams: Math.floor(cal * 0.075),
-                carbsGrams: Math.floor(cal * 0.1),
-                fatsGrams: Math.floor(cal * 0.033),
-                Macros: Macros
-            }
-        case SEND_STATS_FAILED:
-            return{
-                ...state,
-                isFetching: false,
-                error: payload,
-                userStatsObj: {}
-            }
-        default:
-            return state
-    }
-}
+export const userStatsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case FETCH_STATS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: null,
+        userStatsObj: {}
+      };
+    case FETCH_STATS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        userStatsObj: payload.data,
+        totalCalories: payload.calculations.cal,
+        proteinGrams: payload.calculations.protein,
+        carbsGrams: payload.calculations.carbs,
+        fatsGrams: payload.calculations.fats,
+        Macros: payload.calculations.macro
+      };
+    case FETCH_STATS_FAILED:
+      return {
+        ...state,
+        isFetching: false,
+        error: payload,
+        userStatsObj: {}
+      };
+    case SEND_STATS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: null,
+        userStatsObj: {}
+      };
+    case SEND_STATS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: null,
+        userStatsObj: payload.data,
+        totalCalories: payload.calculations.cal,
+        proteinGrams: payload.calculations.protein,
+        carbsGrams: payload.calculations.carbs,
+        fatsGrams: payload.calculations.fats,
+        Macros: payload.calculations.macro
+      };
+    case SEND_STATS_FAILED:
+      return {
+        ...state,
+        isFetching: false,
+        error: payload,
+        userStatsObj: {}
+      };
+    default:
+      return state;
+  }
+};
